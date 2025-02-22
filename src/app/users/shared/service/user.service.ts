153b74238955e77b75d03b47f6aebc7e00b0bc73
Michael Archer <mikearcherdev@proton.me>
2025-02-22 15:15:45 -0600

    Remove accessibility linting and add user model

--- src/app/users/shared/service/user.service.ts
+++ src/app/users/shared/service/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class UserService {
   private http = inject(HttpClient);
   private backendUrl = 'http://localhost:8080/api/admin/create-user';
 
-  registerUser(user: any): Observable<any> {
+  registerUser(user: User): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.http.post(this.backendUrl, user, { headers });
   }
